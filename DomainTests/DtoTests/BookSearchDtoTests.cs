135ce634188d65aa7a0ca931b3263c1fb49b816a
YashojaLakmith <yashoja002@gmail.com>
2024-01-18 18:18:04 +0530

    Began implementing DTO Validation tests

---
+++ DomainTests/DtoTests/BookSearchDtoTests.cs
@@ -0,0 +1,38 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Security.Cryptography;
+
+using Domain.Dto;
+
+using FluentAssertions;
+
+using Xunit;
+
+namespace DomainTests.DtoTests
+{
+    public class BookSearchDtoTests
+    {
+        [Fact]
+        public void Validation_WhenBookNameLengthHigherThan200_ShouldFail()
+        {
+            var randomString = Convert.ToHexString(RandomNumberGenerator.GetBytes(101));
+            var dto = new AdvancedBookSearch(BookName: randomString);
+            var ctx = new ValidationContext(dto);
+
+            var act = () => Validator.ValidateObject(dto, ctx, true);
+
+            act.Should().Throw<ValidationException>();
+        }
+
+        [Fact]
+        public void Validation_WhenAuthorNameLengthHigherThan200_ShouldFail()
+        {
+            var randomString = Convert.ToHexString(RandomNumberGenerator.GetBytes(101));
+            var dto = new AdvancedBookSearch(AuthorName: randomString);
+            var ctx = new ValidationContext(dto);
+
+            var act = () => Validator.ValidateObject(dto, ctx, true);
+
+            act.Should().Throw<ValidationException>();
+        }
+    }
+}
